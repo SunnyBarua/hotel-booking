ea09949e9b9d3b073c8cc163d0d1c3ba1b2d4cec
sunny barua <sunnybarua95@gmail.com>
2021-04-19 20:15:37 +0600

    .gitignore is now working

--- client/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux"
-import {Route,Redirect} from "react-router-dom"
-
-const PrivateRoute=({ ...rest })=>{
-    const {auth}=useSelector((state)=>({...state}));
-
-    return auth && auth.token ? <Route {...rest}/>: <Redirect to="/login"/>;
-}
-export default PrivateRoute;
